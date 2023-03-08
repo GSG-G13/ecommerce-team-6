76b45664531432889824e75d80b1541075a3e605
raghdarizq <raghdarizq2@gmail.com>
2023-03-08 15:08:17 +0200

    testing

---
+++ __tests__/TestingSeller.js
@@ -0,0 +1,13 @@
+const deleteItem = require("../js/helpers");
+describe("delete item", () => {
+  test("should returen true ", () => {
+    const actual = deleteItem([1, 2, 3, 4, 5], 2);
+    const expected = [3];
+    expect(actual).toStrictEqual(expected);
+  });
+  test("should returen true ", () => {
+    const actual = deleteItem([1, 2, 3, 4, 5], 3);
+    const expected = [4];
+    expect(actual).toStrictEqual(expected);
+  });
+});
---
+++ js/helpers.js
@@ -0,0 +1,5 @@
+function deleteItem(array, index) {
+  return array.splice(index, 1);
+}
+
+module.exports = deleteItem;
