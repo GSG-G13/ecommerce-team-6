9dbd608051e0aafaa968806bb02dc4c2303af4fa
raghdarizq <raghdarizq2@gmail.com>
2023-03-08 11:51:57 +0200

    add saller func > Relates #19

---
+++ js/seller.js
@@ -0,0 +1,122 @@
+let tname = document.getElementById("name");
+let price = document.getElementById("price");
+let img = document.getElementById("img");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit-product");
+let sec = document.getElementById("badyTable");
+
+let product;
+let mood = "add";
+let temp;
+
+if (localStorage.product != null) {
+  product = JSON.parse(localStorage.product);
+} else {
+  product = [];
+}
+submit.onclick = function (eo) {
+  eo.preventDefault();
+  let newpro = {
+    name: tname.value,
+    price: price.value,
+    img: img.value,
+    category: category.value,
+    inCart:false,
+  };
+
+  if (mood == "add") {
+    product.push(newpro);
+    console.log(mood);
+  } else {
+    product[temp] = newpro;
+  }
+  localStorage.setItem("product", JSON.stringify(product));
+  readData();
+  clearinp();
+};
+const readData = () => {
+  sec.innerHTML = "";
+  localStorage.setItem("product", JSON.stringify(product));
+
+  product.forEach((p, i) => {
+    // create Main div
+    let div = document.createElement("div");
+    div.className = "table-tbody mt-2";
+
+    // create Image Container
+    let imgContainer = document.createElement("div");
+    imgContainer.className = "td-img";
+
+    // create Image
+    let imgTag = document.createElement("img");
+    imgTag.src = p.img;
+
+    let nameTag = document.createElement("div");
+    let nameText = document.createTextNode(p.name);
+    nameTag.appendChild(nameText);
+    nameTag.className = "td";
+    // price
+    let priceTag = document.createElement("div");
+    priceTag.className = "td-flex";
+    let priceText = document.createTextNode(p.price);
+    priceTag.textContent += "$";
+    priceTag.appendChild(priceText);
+    //category
+    const categoryTag = document.createElement("div");
+    categoryTag.className = "td-flex";
+    let categoryText = document.createTextNode(p.category);
+    categoryTag.appendChild(categoryText);
+    //icon Container
+    const iconContainer = document.createElement("div");
+    iconContainer.className = "td-last justify-content-end";
+    // del
+    const iconDelTag = document.createElement("i");
+    iconDelTag.className = "fa-solid fa-trash";
+    iconDelTag.id = "del";
+    iconDelTag.dataset.index = i;
+    iconDelTag.onclick = onDelete;
+    //edit
+    const iconEditTag = document.createElement("i");
+    iconEditTag.className = "fa-solid fa-pen-to-square";
+    iconEditTag.id = "edit";
+    iconEditTag.dataset.index = i;
+    iconEditTag.onclick = onEdite;
+
+    // append childs
+    iconContainer.appendChild(iconDelTag);
+    iconContainer.appendChild(iconEditTag);
+    imgContainer.appendChild(imgTag);
+    div.appendChild(imgContainer);
+    div.appendChild(nameTag);
+    div.appendChild(priceTag);
+    div.appendChild(categoryTag);
+    div.appendChild(iconContainer);
+    sec.appendChild(div);
+  });
+};
+readData();
+const clearinp = () => {
+  tname.value = "";
+  price.value = "";
+  img.value = "";
+  category.value = "";
+};
+
+function onEdite(e) {
+  mood = "Update";
+  let index = e.target.dataset.index;
+  popup.style.display = "block";
+  overlay.className = "overlay";
+  tname.value = product[index].name;
+  price.value = product[index].price;
+  img.value = product[index].img;
+  category.value = product[index].category;
+  temp = index;
+}
+function onDelete(e) {
+  let index = e.target.dataset.index;
+  console.log(index);
+  product.splice(index, 1);
+
+  readData();
+}
